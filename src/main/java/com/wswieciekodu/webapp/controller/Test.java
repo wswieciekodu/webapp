b6dfc61d1c2f80651fd8964295cdc6564d90b5e8
magda <wswieciekodu@gmail.com>
2024-03-05 13:09:17 +0100

    Test

---
+++ src/main/java/com/wswieciekodu/webapp/controller/Test.java
@@ -0,0 +1,43 @@
+package com.wswieciekodu.webapp.controller;
+
+public class Test {
+    private String test;
+    private boolean testFlag;
+
+    public Test() {
+
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public boolean isTestFlag() {
+        return testFlag;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    ;
+
+    public void setTestFlag(boolean testFlag) {
+        this.testFlag = testFlag;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
